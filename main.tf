7483043de6ed30a53beacae09b509d79e296f641
Laxami Kant <laxmikant010888@gmail.com>
2025-07-31 14:33:28 +0530

    C:\Users\Dell\Desktop\classicpipelinecode

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.rg_location
+}
+
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "rg_name" {
+  description = "Resource Group name"
+  type        = string
+  default     = "demo-tf-rg"
+}
+
+variable "rg_location" {
+  description = "Azure location"
+  type        = string
+  default     = "East US"
+}
